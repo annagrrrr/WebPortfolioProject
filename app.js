2e06dfedf95a3d4770616f9d15d3b579f08057f9
Anna Grigorovich <grigorovichx@gmail.com>
2025-05-24 14:13:48 +0300

    Admin login

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
 
@@ -10,6 +13,20 @@ const PORT = 5000 || process.env.PORT;
 
 connectDB();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'minecraft',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+    cookie: { maxAge: new Date ( Date.now() + (3600000) ) }
+}))
+
 app.use(express.static('public'));
 
 app.use(expressLayout);
@@ -17,6 +34,7 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
